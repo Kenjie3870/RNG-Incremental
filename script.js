cfe5ea356806893c90d15647e26247e62d4502a1
unp©gged™ <oofignacio@gmail.com>
2022-03-09 20:51:25 -0800

    v0.0.1

--- script.js
+++ script.js
@@ -2,80 +2,102 @@
 
 function player() {
     return {
-        points: new Decimal(0),
-        gain: [new Decimal(Math.random()), new Decimal(1), new Decimal(2), new Decimal(0)], // [0] determines whether we get [1] or [2] points
-        upgrades: [[new Decimal(0), new Decimal(5e1), new Decimal(1), new Decimal(2)], 
-                   [new Decimal(0), new Decimal(1.5e3), new Decimal(1), new Decimal(0.5)]] // outermost array displays upgrades, inner array displays amount, cost, effect, and effect base
+        points: new Decimal("0"),
+        gain: {determine: new Decimal(Math.random()), min: new Decimal(1), max: new Decimal(2), final: new Decimal(0)},
+        time: 0,
+        a1: {amount: new Decimal(0), cost: new Decimal(10), scaling: new Decimal(1), effect: new Decimal(0), base: new Decimal(1)},
+        a2: {amount: new Decimal(0), cost: new Decimal(100), scaling: new Decimal(1.2), effect: new Decimal(0), base: new Decimal(0.1)},
+        a3: {amount: new Decimal(0), cost: new Decimal(500), scaling: new Decimal(1.05), effect: new Decimal(1), base: new Decimal(0.15)}
     }
 }
 
-let data = player()
+const data = player()
 
 function update() {
-    if(data.gain[0].gte(0.7)) data.gain[3] = data.gain[2]
-    else if(data.gain[0].lt(0.7)) data.gain[3] = data.gain[1]
-    data.points = data.points.add(data.gain[3])
-    data.gain[0] = new Decimal(Math.random())
+    if(data.gain.determine.gte(0.6)) data.gain.final = data.gain.max
+    else if(data.gain.determine.lt(0.6)) data.gain.final = data.gain.min
+    data.points = data.points.add(data.gain.final)
+    data.gain.determine = new Decimal(Math.random())
 
-    if(data.upgrades[0][0].gte(1)) {data.gain[1] = data.upgrades[0][2], data.gain[2] = new Decimal(2).mul(data.upgrades[0][2])}
-    if(data.upgrades[1][0].gte(1)) {data.upgrades[0][3] = new Decimal(2).add(data.upgrades[1][2])}
+    if(data.a1.amount.gte(1)) data.gain.max = new Decimal(2).add(data.a1.effect).mul(data.a3.amount.gte(1) ? data.a3.effect : new Decimal(1))
+    data.gain.min = data.a3.amount.gte(1) ? data.a3.effect : new Decimal(1)
+    if(data.a2.amount.gte(1)) data.a1.base = new Decimal(1).add(data.a2.effect)
 
-    if(document.getElementById("loading")) document.getElementById("loading").innerHTML = ""
-
-    if(document.getElementById("points")) document.getElementById("points").innerHTML = data.points.toStringWithDecimalPlaces(2)
-    if(document.getElementById("min")) document.getElementById("min").innerHTML = data.gain[1].toStringWithDecimalPlaces(2)
-    if(document.getElementById("max")) document.getElementById("max").innerHTML = data.gain[2].toStringWithDecimalPlaces(2)
-
-    if(document.getElementById("a1-amnt")) document.getElementById("a1-amnt").innerHTML = data.upgrades[0][0].toStringWithDecimalPlaces(2)
-    if(document.getElementById("a1-cost")) document.getElementById("a1-cost").innerHTML = data.upgrades[0][1].toStringWithDecimalPlaces(2)+" points"
-    if(document.getElementById("a1-efft")) document.getElementById("a1-efft").innerHTML = "x"+data.upgrades[0][2].toStringWithDecimalPlaces(2)
-    if(document.getElementById("a1-base")) document.getElementById("a1-base").innerHTML = data.upgrades[0][3].toStringWithDecimalPlaces(2)
-
-    if(document.getElementById("a2-amnt")) document.getElementById("a2-amnt").innerHTML = data.upgrades[1][0].toStringWithDecimalPlaces(2)
-    if(document.getElementById("a2-cost")) document.getElementById("a2-cost").innerHTML = data.upgrades[1][1].toStringWithDecimalPlaces(2)+" points"
-    if(document.getElementById("a2-efft")) document.getElementById("a2-efft").innerHTML = "+"+data.upgrades[1][2].toStringWithDecimalPlaces(2)+", +"+data.upgrades[1][2].mul(0.8).toStringWithDecimalPlaces(2)
-    if(document.getElementById("a2-base")) document.getElementById("a2-base").innerHTML = data.upgrades[1][3].toStringWithDecimalPlaces(2)
+    data.time++
 }
 
-function buyUpgradeA1() {
-    if(data.points.gte(data.upgrades[0][1])) {
-        data.points = data.points.sub(data.upgrades[0][1])
-        data.upgrades[0][0] = data.upgrades[0][0].add(1)
-        data.upgrades[0][1] = new Decimal(data.upgrades[1][0].gte(1) ? new Decimal(3).add(data.upgrades[1][2].mul(0.8)) : 3).pow(data.upgrades[0][0]).mul(5e1)
-        data.upgrades[0][2] = data.upgrades[0][3].pow(data.upgrades[0][0])
+function updateUI() {
+    if(document.getElementById("loading")) document.getElementById("loading").style.display = "none"
 
-        data.gain[1] = data.upgrades[0][2]
-        data.gain[2] = new Decimal(2).mul(data.upgrades[0][2])
-    }
-    else {
-        data.upgrades[0][1] = new Decimal(data.upgrades[1][0].gte(1) ? new Decimal(3).add(data.upgrades[1][2].mul(0.8)) : 3).pow(data.upgrades[0][0]).mul(5e1)
-        data.upgrades[0][2] = data.upgrades[0][3].pow(data.upgrades[0][0])
-    }
+    if(document.getElementById("points")) document.getElementById("points").innerHTML = `You have ${format(data.points)} points.`
+    if(document.getElementById("pointsGain")) document.getElementById("pointsGain").innerHTML = `You get either ${format(data.gain.min)} or ${format(data.gain.max)} every second.`
+    if(document.getElementById("timePlayed")) document.getElementById("timePlayed").innerHTML = `You've spent ${data.time} seconds playing this game.`
+
+    if(document.getElementById("a1-base")) document.getElementById("a1-base").innerHTML = `Add ${format(data.a1.base)} to the second gain.`
+    if(document.getElementById("a1-effect")) document.getElementById("a1-effect").innerHTML = `You've bought ${format(data.a1.amount)} of these, adding ${format(data.a1.effect)} to the second gain. You need ${format(data.a1.cost)} to buy this again.`
+    if(document.getElementById("a2-base")) document.getElementById("a2-base").innerHTML = `Increase A1's base by ${format(data.a2.base)}.`
+    if(document.getElementById("a2-effect")) document.getElementById("a2-effect").innerHTML = `You've bought ${format(data.a2.amount)} of these, increasing A1's base by ${format(data.a2.effect)}. You need ${format(data.a2.cost)} to buy this again.`
+    if(document.getElementById("a3-base")) document.getElementById("a3-base").innerHTML = `Multiply both gains by ${format(data.a3.base)}.`
+    if(document.getElementById("a3-effect")) document.getElementById("a3-effect").innerHTML = `You've bought ${format(data.a3.amount)} of these, multiplying both gains by ${format(data.a3.effect)}. You need ${format(data.a3.cost)} to buy this again.`
+
+    if(document.getElementById("a2")) document.getElementById("a2").style.display = data.a1.amount.gte(6) ? "block" : "none"
+    if(document.getElementById("a3")) document.getElementById("a3").style.display = data.a2.amount.gte(4) ? "block" : "none"
 }
 
-function buyUpgradeA2() {
-    if(data.points.gte(data.upgrades[1][1])) {
-        data.points = data.points.sub(data.upgrades[1][1])
-        data.upgrades[1][0] = data.upgrades[1][0].add(1)
-        data.upgrades[1][1] = new Decimal(10).pow(data.upgrades[1][0]).mul(1.5e3)
-        data.upgrades[1][2] = data.upgrades[1][3].mul(data.upgrades[1][0])
+function buyA1() {
+    let upg = data.a1
+    let points = data.points
+    let gain = data.gain
 
-        data.upgrades[0][1] = new Decimal(data.upgrades[1][0].gte(1) ? new Decimal(3).add(data.upgrades[1][2].mul(0.8)) : 3).pow(data.upgrades[0][0]).mul(5e1)
-        data.upgrades[0][2] = data.upgrades[0][3].pow(data.upgrades[0][0])
-        data.upgrades[0][3] = new Decimal(2).add(data.upgrades[1][2])
-        data.gain[1] = data.upgrades[0][2]
-        data.gain[2] = new Decimal(2).mul(data.upgrades[0][2])
+    if(points.gte(upg.cost)) {
+        data.points = data.points.sub(upg.cost)
+        upg.amount = upg.amount.add(1)
     }
-    else {
-        data.upgrades[0][1] = new Decimal(data.upgrades[1][0].gte(1) ? new Decimal(3).add(data.upgrades[1][2].mul(0.8)) : 3).pow(data.upgrades[0][0]).mul(5e1)
-        data.upgrades[0][2] = data.upgrades[0][3].pow(data.upgrades[0][0])
-        data.upgrades[0][3] = new Decimal(2).add(data.upgrades[1][2])
-        data.upgrades[1][1] = new Decimal(10).pow(data.upgrades[1][0]).mul(1.5e3)
-        data.upgrades[1][2] = data.upgrades[1][3].mul(data.upgrades[1][0])
+    upg.scaling = upg.amount.div(10).add(1)
+    upg.cost = upg.scaling.pow(upg.amount).mul(10)
+    upg.effect = upg.base.mul(upg.amount)
+    gain.max = new Decimal(2).add(upg.effect).mul(data.a3.amount.gte(1) ? data.a3.effect : new Decimal(1))
+    gain.min = data.a3.amount.gte(1) ? data.a3.effect : new Decimal(1)
+}
+
+function buyA2() {
+    let upg = data.a2
+    let affupg = data.a1
+    let points = data.points
+    let gain = data.gain
+
+    if(points.gte(upg.cost)) {
+        data.points = data.points.sub(upg.cost)
+        upg.amount = upg.amount.add(1)
     }
+    upg.scaling = upg.amount.div(9).add(1)
+    upg.cost = upg.scaling.pow(upg.amount).mul(100)
+    upg.effect = upg.base.mul(upg.amount)
+    gain.max = new Decimal(2).add(upg.effect).mul(data.a3.amount.gte(1) ? data.a3.effect : new Decimal(1))
+    gain.min = data.a3.amount.gte(1) ? data.a3.effect : new Decimal(1)
+
+    affupg.base = new Decimal(1).add(upg.effect)
+    affupg.effect = affupg.base.mul(affupg.amount)
+}
+
+function buyA3() {
+    let upg = data.a3
+    let points = data.points
+    let gain = data.gain
+
+    if(points.gte(upg.cost)) {
+        data.points = data.points.sub(upg.cost)
+        upg.amount = upg.amount.add(1)
+    }
+    upg.scaling = upg.amount.div(15).add(1)
+    upg.cost = upg.scaling.pow(upg.amount).mul(500)
+    upg.effect = upg.base.mul(upg.amount).add(1)
+    gain.max = new Decimal(2).add(upg.effect).mul(upg.effect)
+    gain.min = upg.effect
 }
 
 setInterval(update, 1000)
+setInterval(updateUI, 10)
 
 function save() {
     window.localStorage.setItem('this-is-rng', JSON.stringify(data))
@@ -84,6 +106,7 @@ function save() {
 function load() {
     let save = JSON.parse(window.localStorage.getItem('this-is-rng'))
     if(save !== undefined) fixSave(data, save)
+    updateUI()
 }
 
 function fixSave(main=player(), data) {
